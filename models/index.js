27028780d97b7ddbbfd002ba20e1d27428bc3da9
jojo <jodiladouceur.design@gmail.com>
2021-05-18 10:48:09 -0400

    edit the models files

--- models/index.js
+++ models/index.js
@@ -4,13 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', //foreignKey may change
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id', //foreignKey may change
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+})
 
 module.exports = {
   Product,
