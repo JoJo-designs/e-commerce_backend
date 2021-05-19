2879fe272321829b9fc0b16ce37da62bb56bcd76
jojo <jodiladouceur.design@gmail.com>
2021-05-19 19:32:13 -0400

    edits to route files

--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category 
 Product.belongsTo(Category, {
-  foreignKey: 'category_id', //foreignKey may change
+  foreignKey: 'id', //foreignKey may change
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id', //foreignKey may change
+  foreignKey: 'id', //foreignKey may change
 })
 
 // Products belongToMany Tags (through ProductTag)
